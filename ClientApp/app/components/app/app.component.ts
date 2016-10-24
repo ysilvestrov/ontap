25bef4f4f19345ea1e698d4407a6a316dcda615c
Yuriy Silvestrov <yuriy@silvestrov.com>
2016-10-24 23:43:57 +0300

    GA added 2

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginService } from "../login/login.service";
+import { NavigationEnd, Router } from '@angular/router';
+import '../../modules/rxjs.operators';
+
+// Declare ga function as ambient
+declare var ga: Function;
 
 @Component({
     selector: 'app',
@@ -8,4 +13,19 @@ import { LoginService } from "../login/login.service";
     providers: [LoginService],
 })
 export class AppComponent {
+    private currentRoute: string;
+
+    constructor(public router: Router) {
+        router.events.distinctUntilChanged((previous: any, current: any) => {
+            if (current instanceof NavigationEnd) {
+                return previous.url === current.url;
+            }
+            return true;
+        }).subscribe((x: any) => {
+            console.log('router.change', x);
+            if (typeof(ga) != "undefined") {
+                ga || ga('send', 'pageview', x.url);
+            }
+        });
+    }
 }
